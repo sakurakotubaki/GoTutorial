783742e8dddb5b4caecfa9611257fcaaddb37738
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-07-14 21:07:45 +0900

    add じゃんけんするプログラムを作ってみた

--- hello.go
+++ hello.go
@@ -1,36 +1,49 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"math/rand"
+	"os"
+	"strings"
 	"time"
 )
 
+var choices = []string{"✊", "✌️", "✋"}
+
 func main() {
-	// 電気ポットの初期温度
-	currentTemperature := 20
+	// 乱数のシードを設定
+	rand.Seed(time.Now().UnixNano())
 
-	// 目標温度
-	targetTemperature := 100
+	reader := bufio.NewReader(os.Stdin)
 
-	// カチッと音を鳴らす関数
-	playSound := func() {
-		fmt.Println("カチッ")
-	}
+	fmt.Println("じゃんけんゲームを始めます！")
+	fmt.Println("✊: グー, ✌️: チョキ, ✋: パー")
+	fmt.Print("選択してください（✊, ✌️, ✋）: ")
 
-	// 温度が目標温度に達するまでループ
-	for currentTemperature < targetTemperature {
-		// 温度を上昇させる処理（例：1度ずつ上昇させる）
-		currentTemperature++
+	// プレイヤーの選択
+	playerChoice, _ := reader.ReadString('\n')
+	playerChoice = strings.TrimSpace(playerChoice)
 
-		// 1度上昇するごとに温度を表示
-		fmt.Printf("現在の温度：%d度\n", currentTemperature)
+	// コンピュータの選択
+	computerChoice := choices[rand.Intn(len(choices))]
 
-		// お湯が100度になったらカチッと音を鳴らす
-		if currentTemperature == targetTemperature {
-			playSound()
-		}
+	fmt.Println("あなた:", playerChoice)
+	fmt.Println("コンピュータ:", computerChoice)
 
-		// 1秒待機
-		time.Sleep(time.Second)
+	// 結果の判定
+	result := judge(playerChoice, computerChoice)
+	fmt.Println("結果:", result)
+}
+
+func judge(playerChoice, computerChoice string) string {
+	if playerChoice == computerChoice {
+		return "引き分け"
+	} else if (playerChoice == "✊" && computerChoice == "✌️") ||
+		(playerChoice == "✌️" && computerChoice == "✋") ||
+		(playerChoice == "✋" && computerChoice == "✊") {
+		return "あなたの勝ち"
+	} else {
+		return "コンピュータの勝ち"
 	}
 }
